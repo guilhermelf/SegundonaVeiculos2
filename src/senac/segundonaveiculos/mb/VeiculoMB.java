b36b206195e34a0c5835825467bba3d721364c11
unknown <631310076@SALA603-08.fspoaeduc.com.br>
2015-05-15 21:17:52 -0300

    Atualizacao de pacotes

---
+++ src/senac/segundonaveiculos/mb/VeiculoMB.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package senac.segundonaveiculos.mb;
+
+/**
+ *
+ * @author 631310076
+ */
+public class VeiculoMB {
+
+    private Integer id;
+    private String ano;
+    private String cor;
+    private String marca;
+    private String motor;
+    private String nome;
+    private Integer portas;
+    private Float preco;
+    private Integer valvulas;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPortas() {
+        return portas;
+    }
+
+    public void setPortas(Integer portas) {
+        this.portas = portas;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+
+    public Integer getValvulas() {
+        return valvulas;
+    }
+
+    public void setValvulas(Integer valvulas) {
+        this.valvulas = valvulas;
+    }
+
+}
